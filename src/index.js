6be063c69e4d0e7482af4625fcc4405ccbb9152f
Leigh Michael Forrest <leigh@catbirdseat.io>
2023-01-12 19:23:46 -0500

    menu ready

---
+++ src/index.js
@@ -0,0 +1,9 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+    const toggle = document.querySelector("toggle");
+    const showcase = document.querySelector(".showcase");
+
+    toggle.addEventListener("click", ()=> {
+        toggle.classList.toggle("active");
+        showcase.classList.toggle("active");
+    })
+});
